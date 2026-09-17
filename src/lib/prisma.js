edfd1dedfba8e2945e0c1ae41d5181ab70a32f97
Triple D <danieldurojaiye42@gmail.com>
2026-09-17 10:46:37 +0100

    Update schema.prisma with binaryTargets for Linux and Windows

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,4 +1,4 @@
-const { PrismaClient } = require('../generated/client');
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = global.prisma || new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
