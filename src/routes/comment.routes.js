801a614b7906495c73a3feff3d31b5de549d4f9b
Triple D <danieldurojaiye42@gmail.com>
2026-09-17 12:09:20 +0100

    Add GET /api/comments/test route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { optionalAuth } = require('../middleware/auth');
 const { toggleCommentLike } = require('../controllers/post.controller');
 
+router.get('/test', (req, res) => res.json({ status: 'ok' }));
 router.post('/:commentId/like', optionalAuth, (req, res, next) => {
   toggleCommentLike(req, res).catch(next);
 });
